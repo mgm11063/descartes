f752dc3e7823147d8e44685e1183f9cd53fe2f87
문경민 <mgm11063@gmail.com>
2022-04-13 16:16:14 +0900

    save for home

--- companies/views.py
+++ companies/views.py
@@ -5,21 +5,13 @@ from django.http import JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
 
-# def company_detail(request, pk):
-#     company = models.Company.objects.get(pk=pk)
-
-#     return render(
-#         request,
-#         "companies/company_detail.html",
-#         context={"company": company},
-#     )
-
 
 @csrf_exempt
 def company_detail(request, pk):
     if request.method == "POST":
         data = json.loads(request.body)
         # do something
+
         print(data)
 
         context = {
