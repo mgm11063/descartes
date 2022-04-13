27c12560daa6b93da2cc08f97ef1a38a9509387f
문경민 <mgm11063@gmail.com>
2022-04-13 20:09:38 +0900

    [ 기능] db프론트와 기초적인 연동  완료

--- companies/views.py
+++ companies/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from . import models
 import json
-from django.http import JsonResponse
 
+from django.core import serializers
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def company_detail(request, pk):
     if request.method == "POST":
-        data = json.loads(request.body)
-        # do something
+        item_id = json.loads(request.body)
+        sections = models.Section.objects.all().filter(pk=item_id)
+        item_data = serializers.serialize("json", sections)
 
-        print(data)
-
-        context = {
-            "result": data,
-        }
-        return JsonResponse(context)
+        response = HttpResponse(content=item_data)
+        return response
 
     elif request.method == "GET":
         company = models.Company.objects.get(pk=pk)
